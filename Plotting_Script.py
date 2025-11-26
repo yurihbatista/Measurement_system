d673ae0f3f2ecdcda671d4667572ac00e4bc891c
yurihbatista <henriques.yuri@gmail.com>
2025-11-25 23:07:02 -0300

    Added second HF channel and the two LF channels, new packet for UART data and updated plotting python script.

--- Plotting_Script.py
+++ Plotting_Script.py
@@ -4,31 +4,32 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
 from collections import deque
-import threading  
-import queue      
-import time      
+import threading
+import queue
+import time
+import struct
 
-
-#Config
-#    SERIAL_PORT = sys.argv[1]
-#    print("Error: Please provide the serial port as an argument.")
-#    sys.exit(1)
+# Config
 SERIAL_PORT = '/dev/ttyACM0'
 
-#Serial
+# Serial
 SERIAL_BAUD = 4000000
+START_WORD = 0xAAAA
+END_WORD = 0xBBBB
+HEADER_FORMAT = '<HBH' # Start word (uint16), Channel ID (uint8), Data size (uint16)
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+END_WORD_SIZE = struct.calcsize('<H')
 
-SYNC_WORD = 0xAAAA
 
-#Visualization
+# Visualization
 SAMPLES_TO_DISPLAY = 256 * 6
-MAX_VOLTAGE_MV = 2600
+MAX_VOLTAGE_MV = 3300
 MIN_VOLTAGE_MV = 0
 
-#Trigger
+# Trigger
 TRIGGER_LEVEL_MV = 1275
-TIMER_INTERVAL = 0 
-TRIGGER_RANGE =3 
+TIMER_INTERVAL = 0
+TRIGGER_RANGE = 3
 
 class myPlotter:
     def __init__(self):
@@ -40,23 +41,59 @@ class myPlotter:
 
         self.app = QtWidgets.QApplication([])
         self.win = pg.GraphicsLayoutWidget(title="ESP32 Oscilloscope (Multithreaded)")
-        self.win.resize(1200, 600)
-        self.plot = self.win.addPlot(title="Live ADC Data")
-        self.plot.setYRange(MIN_VOLTAGE_MV, MAX_VOLTAGE_MV)
-        self.plot.setXRange(0, SAMPLES_TO_DISPLAY)
-        self.plot.setLabel('left', 'Voltage (mV)')
-        self.plot.setLabel('bottom', 'Sample')
-        self.plot.showGrid(x=True, y=True)
-        self.plot.addLine(y=TRIGGER_LEVEL_MV, pen=pg.mkPen('g', style=QtCore.Qt.PenStyle.DashLine))
-        self.curve = self.plot.plot(pen='y')
+        self.win.resize(1200, 800)
+
+        # Plot for Channel 0
+        self.plot0 = self.win.addPlot(title="Channel 0", row=0, col=0)
+        self.plot0.setYRange(MIN_VOLTAGE_MV, MAX_VOLTAGE_MV)
+        self.plot0.setXRange(0, SAMPLES_TO_DISPLAY)
+        self.plot0.setLabel('left', 'Voltage (mV)')
+        self.plot0.setLabel('bottom', 'Sample')
+        self.plot0.showGrid(x=True, y=True)
+        self.plot0.addLine(y=TRIGGER_LEVEL_MV, pen=pg.mkPen('g', style=QtCore.Qt.PenStyle.DashLine))
+        self.curve0 = self.plot0.plot(pen='y')
+
+        # Plot for Channel 1
+        self.plot1 = self.win.addPlot(title="Channel 1", row=1, col=0)
+        self.plot1.setYRange(MIN_VOLTAGE_MV, MAX_VOLTAGE_MV)
+        self.plot1.setXRange(0, SAMPLES_TO_DISPLAY)
+        self.plot1.setLabel('left', 'Voltage (mV)')
+        self.plot1.setLabel('bottom', 'Sample')
+        self.plot1.showGrid(x=True, y=True)
+        self.plot1.addLine(y=TRIGGER_LEVEL_MV, pen=pg.mkPen('c', style=QtCore.Qt.PenStyle.DashLine))
+        self.curve1 = self.plot1.plot(pen='c')
+        
+        # Plot for Channel 2
+        self.plot2 = self.win.addPlot(title="Channel 2", row=0, col=1)
+        self.plot2.setYRange(MIN_VOLTAGE_MV, MAX_VOLTAGE_MV)
+        self.plot2.setLabel('left', 'Voltage (mV)')
+        self.plot2.setLabel('bottom', 'Sample')
+        self.plot2.showGrid(x=True, y=True)
+        self.curve2 = self.plot2.plot(pen='m')
+
+        # Plot for Channel 3
+        self.plot3 = self.win.addPlot(title="Channel 3", row=1, col=1)
+        self.plot3.setYRange(MIN_VOLTAGE_MV, MAX_VOLTAGE_MV)
+        self.plot3.setLabel('left', 'Voltage (mV)')
+        self.plot3.setLabel('bottom', 'Sample')
+        self.plot3.showGrid(x=True, y=True)
+        self.curve3 = self.plot3.plot(pen='w')
+
+        self.win.ci.layout.setColumnStretchFactor(0, 2)
+        self.win.ci.layout.setColumnStretchFactor(1, 1)
+
 
         max_buf_len = SAMPLES_TO_DISPLAY * 3
-        self.data_buffer = deque(maxlen=max_buf_len)
+        self.data_buffer0 = deque(maxlen=max_buf_len)
+        self.data_buffer1 = deque(maxlen=max_buf_len)
+        self.data_buffer2 = deque(maxlen=100) # Smaller buffer for low frequency channels
+        self.data_buffer3 = deque(maxlen=100)
 
-        self.data_queue = queue.Queue() 
-        self.stop_event = threading.Event() 
+
+        self.data_queue = queue.Queue()
+        self.stop_event = threading.Event()
         self.reader_thread = threading.Thread(target=self.serial_reader)
-        self.reader_thread.daemon = True 
+        self.reader_thread.daemon = True
         self.reader_thread.start()
 
         self.app.aboutToQuit.connect(self.cleanup)
@@ -70,42 +107,95 @@ class myPlotter:
         self.app.exec()
 
     def serial_reader(self):
+        read_buffer = bytearray()
         while not self.stop_event.is_set():
-            bytes_to_read = self.ser.in_waiting
-            if bytes_to_read > 0:
-                data = self.ser.read(bytes_to_read)
-                self.data_queue.put(data)
-            else:
-                time.sleep(0.001)
+            try:
+                data = self.ser.read(self.ser.in_waiting or 1)
+                if data:
+                    read_buffer.extend(data)
+                    
+                    while True:
+                        start_index = read_buffer.find(struct.pack('<H', START_WORD))
+                        if start_index == -1:
+                            break 
+
+                        read_buffer = read_buffer[start_index:]
+
+                        if len(read_buffer) < HEADER_SIZE:
+                            break
+
+                        header = read_buffer[:HEADER_SIZE]
+                        _, channel_id, data_size = struct.unpack(HEADER_FORMAT, header)
+
+                        if len(read_buffer) < HEADER_SIZE + data_size + END_WORD_SIZE:
+                            break
+
+                        end_word_index = HEADER_SIZE + data_size
+                        end_word_bytes = read_buffer[end_word_index : end_word_index + END_WORD_SIZE]
+                        
+                        if len(end_word_bytes) < END_WORD_SIZE:
+                           break
+
+                        end_word = struct.unpack('<H', end_word_bytes)[0]
+
+                        if end_word == END_WORD:
+                            packet_data = read_buffer[HEADER_SIZE:end_word_index]
+                            raw_values = np.frombuffer(packet_data, dtype=np.uint16)
+                            self.data_queue.put((channel_id, raw_values))
+                            read_buffer = read_buffer[end_word_index + END_WORD_SIZE:]
+                        else:
+                            read_buffer = read_buffer[1:]
+
+
+            except Exception as e:
+                print(f"Error in serial_reader: {e}")
+                time.sleep(0.01)
 
     def update(self):
-        """Pulls data from the queue and updates the plot."""
         try:
             while not self.data_queue.empty():
-                data_bytes = self.data_queue.get_nowait()
-                if len(data_bytes) % 2 == 0:
-                        raw_values = np.frombuffer(data_bytes, dtype=np.uint16)
-                        if len(raw_values) > 0:
-                            self.data_buffer.extend(raw_values)
+                channel_id, raw_values = self.data_queue.get_nowait()
+                if channel_id == 0:
+                    self.data_buffer0.extend(raw_values)
+                elif channel_id == 1:
+                    self.data_buffer1.extend(raw_values)
+                elif channel_id == 2:
+                    self.data_buffer2.extend(raw_values)
+                elif channel_id == 3:
+                    self.data_buffer3.extend(raw_values)
+
         except queue.Empty:
             pass
 
-        if len(self.data_buffer) < SAMPLES_TO_DISPLAY + 10:
+        self.update_plot_triggered(self.data_buffer0, self.curve0)
+        self.update_plot_triggered(self.data_buffer1, self.curve1)
+        self.update_plot_simple(self.data_buffer2, self.curve2)
+        self.update_plot_simple(self.data_buffer3, self.curve3)
+
+    def update_plot_triggered(self, data_buffer, curve):
+        if len(data_buffer) < SAMPLES_TO_DISPLAY + TRIGGER_RANGE:
             return
 
-        buffer_np = np.array(self.data_buffer, dtype=np.uint16)
+        buffer_np = np.array(data_buffer, dtype=np.uint16)
+        
+        # Simple trigger - find first rising edge
         candidates = np.where((buffer_np[:-TRIGGER_RANGE] < TRIGGER_LEVEL_MV) & (buffer_np[TRIGGER_RANGE:] >= TRIGGER_LEVEL_MV))[0]
-
+        
         if len(candidates) > 0:
             trigger_point = candidates[0] + 1
             if len(buffer_np) >= trigger_point + SAMPLES_TO_DISPLAY:
-                waveform = buffer_np[trigger_point : trigger_point + SAMPLES_TO_DISPLAY]
-                self.curve.setData(waveform)
+                waveform = buffer_np[trigger_point:trigger_point + SAMPLES_TO_DISPLAY]
+                curve.setData(waveform)
+                # Remove used data from buffer
                 for _ in range(trigger_point):
-                    self.data_buffer.popleft()
+                    data_buffer.popleft()
+
+    def update_plot_simple(self, data_buffer, curve):
+        if len(data_buffer) > 0:
+            curve.setData(np.array(data_buffer))
+
 
     def cleanup(self):
-        """Signals the reader thread to stop and closes the serial port."""
         print("Cleaning up...")
         self.stop_event.set()
         self.reader_thread.join(timeout=1)
@@ -114,4 +204,4 @@ class myPlotter:
 
 if __name__ == '__main__':
     plotter = myPlotter()
-    sys.exit() 
+    sys.exit()
--- src/main.c
+++ src/main.c
@@ -8,12 +8,19 @@
 #include "driver/uart.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+#include "esp_timer.h"
+#include "esp_adc/adc_oneshot.h"
 
 static const char *TAG = "INTERRUPT_SCOPE";
 
 #define DOWNSAMPLING_FACTOR 8
-#define ADC_INPUT_CHANNEL   ADC_CHANNEL_6 // GPIO34
-#define ADC_SAMPLE_RATE     400000
+#define ADC_INPUT_CHANNEL_0   ADC_CHANNEL_6 // GPIO34
+#define ADC_INPUT_CHANNEL_1   ADC_CHANNEL_7 //GPIO35
+#define ADC2_INPUT_CHANNEL_0  ADC_CHANNEL_8 // GPIO25
+#define ADC2_INPUT_CHANNEL_1  ADC_CHANNEL_9 // GPIO26
+
+#define ADC_SAMPLE_RATE     100000
+#define ADC2_SAMPLE_RATE_HZ 100
 #define SERIAL_BAUD_RATE    4000000
 #define ADC_READ_LEN        (256 * 8)
 
@@ -22,16 +29,32 @@ const uint16_t SAMPLES_PER_BLOCK = ADC_READ_LEN / SOC_ADC_DIGI_RESULT_BYTES;
 
 static adc_continuous_handle_t adc_handle = NULL;
 static adc_cali_handle_t adc_cali_handle = NULL;
+static adc_oneshot_unit_handle_t adc2_handle;
 static bool adc_calibrated = false;
 
 static QueueHandle_t s_adc_queue;
+static QueueHandle_t uart_queue;
+static TaskHandle_t s_adc2_task_handle;
+
+
+typedef struct {
+    uint16_t start_word;
+    uint8_t channel_id;
+    uint16_t data_size;
+} __attribute__((packed)) PacketHeader;
+
+const uint16_t END_WORD = 0xBBBB;
 
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data) {
     BaseType_t must_yield = pdFALSE;
-
     xQueueSendFromISR(s_adc_queue, edata, &must_yield);
-
     return (must_yield == pdTRUE);
+}
+
+static void adc2_timer_callback(void* arg) {
+    if (s_adc2_task_handle != NULL) {
+        xTaskNotifyGive(s_adc2_task_handle);
+    }
 }
 
 static bool check_and_init_adc_calibration(adc_unit_t adc_unit, adc_cali_handle_t *out_handle) {
@@ -50,8 +73,9 @@ static bool check_and_init_adc_calibration(adc_unit_t adc_unit, adc_cali_handle_
 }
 
 static void oscilloscope_task(void *arg) {
-    uint16_t *tx_buffer = (uint16_t *)malloc(SAMPLES_PER_BLOCK * sizeof(uint16_t));
-    if (!tx_buffer) {
+    uint16_t *tx_buffer_0 = (uint16_t *)malloc((SAMPLES_PER_BLOCK/2) * sizeof(uint16_t));
+    uint16_t *tx_buffer_1 = (uint16_t *)malloc((SAMPLES_PER_BLOCK/2) * sizeof(uint16_t));
+    if (!tx_buffer_0 || !tx_buffer_1) {
         ESP_LOGE(TAG, "Failed to allocate memory for tx_buffer");
         vTaskDelete(NULL);
         return;
@@ -67,53 +91,121 @@ static void oscilloscope_task(void *arg) {
             uint8_t *raw_adc_buffer = evt_data.conv_frame_buffer;
             uint32_t samples_read = evt_data.size / SOC_ADC_DIGI_RESULT_BYTES;
 
-            uint32_t downsampled_samples_count = 0;
+            uint32_t downsampled_samples_count[2] = {0};
             for (int i = 0; i < samples_read; i += DOWNSAMPLING_FACTOR) {
-                uint32_t sum = 0;
+                uint32_t sum[2] = {0};
+                uint32_t count[2] = {0};
                 for (int j = 0; j < DOWNSAMPLING_FACTOR && (i + j) < samples_read; j++) {
                     adc_digi_output_data_t *p = (adc_digi_output_data_t*)&raw_adc_buffer[(i + j) * SOC_ADC_DIGI_RESULT_BYTES];
                     int voltage_mv = 0;
-                    if (adc_calibrated) {
-                        adc_cali_raw_to_voltage(adc_cali_handle, p->type1.data, &voltage_mv);
-                    } else {
-                        voltage_mv = p->type1.data; 
+                    if(p->type1.channel == ADC_INPUT_CHANNEL_0) {
+                          adc_cali_raw_to_voltage(adc_cali_handle, p->type1.data, &voltage_mv);
+                          sum[0] += voltage_mv;
+                          count[0]++;
                     }
-                    sum += voltage_mv;
+                    if(p->type1.channel == ADC_INPUT_CHANNEL_1) {
+                          adc_cali_raw_to_voltage(adc_cali_handle, p->type1.data, &voltage_mv);
+                          sum[1] += voltage_mv;
+                          count[1]++;
+                    }
                 }
-                tx_buffer[downsampled_samples_count++] = (uint16_t)(sum / DOWNSAMPLING_FACTOR);
+                if(count[0]) tx_buffer_0[downsampled_samples_count[0]++] = (uint16_t)(sum[0] / count[0]);
+                if(count[1]) tx_buffer_1[downsampled_samples_count[1]++] = (uint16_t)(sum[1] / count[1]);
             }
 
-            uart_write_bytes(UART_NUM_0, (const char*)tx_buffer, downsampled_samples_count * sizeof(uint16_t));
+            PacketHeader header0 = {
+                .start_word = SYNC_WORD,
+                .channel_id = 0,
+                .data_size = downsampled_samples_count[0] * sizeof(uint16_t)
+            };
+            uart_write_bytes(UART_NUM_0, (const char*)&header0, sizeof(PacketHeader));
+            uart_write_bytes(UART_NUM_0, (const char*)tx_buffer_0, header0.data_size);
+            uart_write_bytes(UART_NUM_0, (const char*)&END_WORD, sizeof(END_WORD));
+            
+            PacketHeader header1 = {
+                .start_word = SYNC_WORD,
+                .channel_id = 1,
+                .data_size = downsampled_samples_count[1] * sizeof(uint16_t)
+            };
+            uart_write_bytes(UART_NUM_0, (const char*)&header1, sizeof(PacketHeader));
+            uart_write_bytes(UART_NUM_0, (const char*)tx_buffer_1, header1.data_size);
+            uart_write_bytes(UART_NUM_0, (const char*)&END_WORD, sizeof(END_WORD));
         }
     }
-    free(tx_buffer);
+    free(tx_buffer_0);
+    free(tx_buffer_1);
     vTaskDelete(NULL);
 }
 
+static void adc2_task(void *arg)
+{
+    while(1) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        int adc_raw[2];
+        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC2_INPUT_CHANNEL_0, &adc_raw[0]));
+        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, ADC2_INPUT_CHANNEL_1, &adc_raw[1]));
+        
+        uint16_t adc_val_0 = (uint16_t)adc_raw[0];
+        uint16_t adc_val_1 = (uint16_t)adc_raw[1];
+
+        PacketHeader header2 = {
+            .start_word = SYNC_WORD,
+            .channel_id = 2,
+            .data_size = sizeof(uint16_t)
+        };
+        uart_write_bytes(UART_NUM_0, (const char*)&header2, sizeof(PacketHeader));
+        uart_write_bytes(UART_NUM_0, (const char*)&adc_val_0, header2.data_size);
+        uart_write_bytes(UART_NUM_0, (const char*)&END_WORD, sizeof(END_WORD));
+
+        PacketHeader header3 = {
+            .start_word = SYNC_WORD,
+            .channel_id = 3,
+            .data_size = sizeof(uint16_t)
+        };
+        uart_write_bytes(UART_NUM_0, (const char*)&header3, sizeof(PacketHeader));
+        uart_write_bytes(UART_NUM_0, (const char*)&adc_val_1, header3.data_size);
+        uart_write_bytes(UART_NUM_0, (const char*)&END_WORD, sizeof(END_WORD));
+    }
+}
+
+
 void app_main(void) {
-    esp_log_level_set("*", ESP_LOG_NONE);
+    esp_log_level_set("*", ESP_LOG_INFO);
     adc_calibrated = check_and_init_adc_calibration(ADC_UNIT_1, &adc_cali_handle);
 
     uart_config_t uart_config = {
         .baud_rate = SERIAL_BAUD_RATE, .data_bits = UART_DATA_8_BITS, .parity = UART_PARITY_EVEN,
         .stop_bits = UART_STOP_BITS_1, .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, .source_clk = UART_SCLK_DEFAULT,
     };
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 4096, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 4096, 4096, 5, &uart_queue, 0));
     ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
 
-    s_adc_queue = xQueueCreate(2, sizeof(adc_continuous_evt_data_t));
+    s_adc_queue = xQueueCreate(4, sizeof(adc_continuous_evt_data_t));
 
-    adc_continuous_handle_cfg_t adc_config = { .max_store_buf_size = 4096, .conv_frame_size = ADC_READ_LEN, };
+    adc_continuous_handle_cfg_t adc_config = { .max_store_buf_size = ((ADC_READ_LEN/2)*16), .conv_frame_size = ADC_READ_LEN, };
     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &adc_handle));
     
-    adc_digi_pattern_config_t adc_pattern = {
-        .atten = ADC_ATTEN_DB_12, .channel = ADC_INPUT_CHANNEL, .unit = ADC_UNIT_1,
-        .bit_width = ADC_BITWIDTH_12,
-    };
+    adc_digi_pattern_config_t adc_pattern[2] = {0};
+
+    adc_pattern[0].atten = ADC_ATTEN_DB_12;
+    adc_pattern[0].channel = ADC_INPUT_CHANNEL_0;
+    adc_pattern[0].unit = ADC_UNIT_1;
+    adc_pattern[0].bit_width = ADC_BITWIDTH_12;
+
+    adc_pattern[1].atten = ADC_ATTEN_DB_12;
+    adc_pattern[1].channel = ADC_INPUT_CHANNEL_1;
+    adc_pattern[1].unit = ADC_UNIT_1;
+    adc_pattern[1].bit_width = ADC_BITWIDTH_12;
+
     adc_continuous_config_t dig_cfg = {
-        .sample_freq_hz = ADC_SAMPLE_RATE, .conv_mode = ADC_CONV_SINGLE_UNIT_1,
-        .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1, .pattern_num = 1, .adc_pattern = &adc_pattern,
+        .sample_freq_hz = ADC_SAMPLE_RATE, 
+        .conv_mode = ADC_CONV_SINGLE_UNIT_1,
+        .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1, 
+        .pattern_num = 2, 
+        .adc_pattern = adc_pattern,
     };
+
     ESP_ERROR_CHECK(adc_continuous_config(adc_handle, &dig_cfg));
 
     adc_continuous_evt_cbs_t cbs = {
@@ -122,7 +214,33 @@ void app_main(void) {
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(adc_handle, &cbs, NULL));
 
     ESP_ERROR_CHECK(adc_continuous_start(adc_handle));
-    xTaskCreatePinnedToCore(oscilloscope_task, "scope_task", 4096, NULL, 5, NULL, 1);
+
+    // ADC2 setup
+    adc_oneshot_unit_init_cfg_t init_config2 = {
+        .unit_id = ADC_UNIT_2,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_12,
+        .atten = ADC_ATTEN_DB_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC2_INPUT_CHANNEL_0, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, ADC2_INPUT_CHANNEL_1, &config));
+
+
+    xTaskCreatePinnedToCore(oscilloscope_task, "scope_task", 4096, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(adc2_task, "adc2_task", 2048, NULL, 5, &s_adc2_task_handle, 1);
+
+    const esp_timer_create_args_t periodic_timer_args = {
+            .callback = &adc2_timer_callback,
+            .name = "adc2_timer"
+    };
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000 / ADC2_SAMPLE_RATE_HZ));
+
     
     ESP_LOGI(TAG, "Initialization complete. Streaming INTERRUPT-DRIVEN data.");
 }
